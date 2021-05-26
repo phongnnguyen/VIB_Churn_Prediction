53270a0c9a0ac7e2b3e0e1dc55aa9f437cf97f24
phongNg <>
2021-05-26 21:42:37 +0700

    -Complete label for data -structure folder

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # VIB_Churn_Prediction
 The repository for VIB context 
+project_name
+├── data
+│   ├── myVIB.csv
+├── model
+│   ├── suppervisor classify
+│   ├── unsuppervisor classify
+├── src
+│   └── package_name
+│       └── __init__.py
+├── tests
+│   └── __init__.py
+├── .gitignore
+├── LICENSE.txt
+├── MANIFEST.in
+├── README.rst
+├── requirements.txt
+├── setup.cfg
+├── setup.py
+└── tox.ini
--- preprocessing/pre_processing.py
+++ preprocessing/pre_processing.py
@@ -1,16 +1,22 @@
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, timedelta
 
 class Processing_Data:
     def __init__(self,DataPath):
         self.DataPath = DataPath
-
+    def remove_outlier(self):
+        print('here')
+    def remove_duplicate(self):
+        print('here')
+    def combine_data_from_multiple_table(self):
+        print('here')
 
 class Define_Churn_Users:
     '''Define what are churned users'''
-    def __init__(self, df_activity):
+    def __init__(self, df_activity,endDate):
         self.df_activity = df_activity
+        self.endDate = endDate
 
     def calculate_number_inactive_all_users(self):
         '''Calculate the inactive day between 2 active day for each user'''
@@ -28,10 +34,69 @@ class Define_Churn_Users:
 
         return number_of_inactive
 
-    def highest_day_inactive(self,init_noDate = None):
-        if init_noDate is not None:
-            print('Here')
+    def calculate_number_inactive_from_last_purchase(self):
+        last_day_of_year = self.endDate
+        # get last purchase date
+        last_purchase_customer = self.df_activity.sort_values(by="ACTIVITY_DATE").drop_duplicates(
+            subset=["CUSTOMER_NUMBER"], keep="last")
+        last_purchase_customer.index = last_purchase_customer['CUSTOMER_NUMBER']
+
+        # Calculate number inactive day from last purchase of user
+        last_purchase_customer['LAST_PURCHASE'] = (last_day_of_year - pd.to_datetime(
+            last_purchase_customer['ACTIVITY_DATE'])).dt.days - 1
+        last_purchase_customer['LAST_PURCHASE'][last_purchase_customer['LAST_PURCHASE'] < 0] = 0
+
+        return last_purchase_customer
 
 
-    def label_churn_for_user(self):
-        print('here')
+    def label_churn_for_user(self,last_purchase_day_num, init_noDate = 10):
+        # Initial label churn or not churn
+        last_purchase_day_num['LABEL'] = 0
+        last_purchase_day_num['LABEL'][last_purchase_day_num['LAST_PURCHASE'] > 12] = 1
+
+        S = self.endDate - timedelta(days=12)
+        df_before_S =  self.df_activity [pd.to_datetime(last_purchase_day_num['ACTIVITY_DATE'], infer_datetime_format=True) < S]
+
+        last_purchase_before_S = df_before_S.sort_values(by="ACTIVITY_DATE").drop_duplicates(subset=["CUSTOMER_NUMBER"],
+                                                                                             keep="last")
+
+        d = range(1, 30)
+        total_error = []
+        for i in d:
+            Sd = S - timedelta(days=i)
+            purchase_before_Sd = last_purchase_before_S[pd.to_datetime(last_purchase_before_S['ACTIVITY_DATE'],
+                                                                       infer_datetime_format=True) < Sd]
+
+            purchase_between_Sd = last_purchase_before_S[(pd.to_datetime(last_purchase_before_S['ACTIVITY_DATE'],
+                                                                         infer_datetime_format=True) >= Sd)
+                                                         & (pd.to_datetime(last_purchase_before_S['ACTIVITY_DATE'],
+                                                                           infer_datetime_format=True) < S)]
+            count_label = last_purchase_day_num.loc[purchase_before_Sd['CUSTOMER_NUMBER']]['LABEL'].value_counts()
+            label_churn = count_label[1]
+            label_nonchurn = count_label[0]
+
+            count_label_between = last_purchase_day_num.loc[purchase_between_Sd['CUSTOMER_NUMBER']]['LABEL'].value_counts()
+            try:
+                label_churn_between = count_label_between[1]
+            except:
+                label_churn_between = 0
+
+            label_nonchurn_between = count_label_between[0]
+
+            epsilon_FP = label_nonchurn / \
+                         last_purchase_day_num.loc[last_purchase_before_S['CUSTOMER_NUMBER']]['LABEL'].value_counts()[0]
+            epsilon_FN = label_churn_between / \
+                         last_purchase_day_num.loc[last_purchase_before_S['CUSTOMER_NUMBER']]['LABEL'].value_counts()[1]
+
+            total_error.append(0.5 * epsilon_FP + 0.5 * epsilon_FN)
+
+        highest_number_of_inactive = np.argmin(np.array(total_error))
+
+        # re-lable with new threshold
+
+        last_purchase_day_num['LABEL'] = 0
+        last_purchase_day_num['LABEL'][
+            last_purchase_day_num['LAST_PURCHASE'] > init_noDate + highest_number_of_inactive] = 1
+        last_purchase_day_num.to_csv('data/Combine_Data_VIB.csv')
+        return last_purchase_day_num
+
